07986d1d135297303bdef20daeb6e1ca19080194
Dayles1 <sololeveling0025@gmail.com>
2025-02-20 16:48:48 +0500

    +ReqauestClass and add user_id and description to Post m

--- database/migrations/2025_02_17_100413_create_posts_table.php
+++ database/migrations/2025_02_17_100413_create_posts_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->timestamps();
-        });
+       
+            Schema::create('posts', function (Blueprint $table) {
+                $table->id();
+                $table->string('title');
+                $table->text('content');
+                $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+                $table->timestamps();
+            });
+        
+        
     }
 
     /**
