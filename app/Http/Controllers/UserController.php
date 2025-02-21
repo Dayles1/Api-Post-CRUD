d6f17396d3ba362df799b4a0abfe01e193a3dfe6
Dayles1 <sololeveling0025@gmail.com>
2025-02-21 11:44:27 +0500

    +index|store user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
-    
+    // User yaratish (Create)
+    public function store(RegisterRequest $request)
+    {
+       
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+  
+}
+
 }
