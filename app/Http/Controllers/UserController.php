45d00fa07a52bf1b3accbede1f27638cebef4a17
Dayles1 <sololeveling0025@gmail.com>
2025-02-21 11:45:07 +0500

    +show user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,17 @@ class UserController extends Controller
         return response()->json(User::all());
     }
 
-  
-}
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
 
 }
+
+
