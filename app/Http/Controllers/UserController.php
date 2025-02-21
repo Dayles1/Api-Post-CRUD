43b42016c99e6a582ab41ca6d93441d733c3ddff
Dayles1 <sololeveling0025@gmail.com>
2025-02-21 11:48:15 +0500

    User destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,7 +65,18 @@ class UserController extends Controller
         return response()->json($user);
     }
 
-   
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
 }
 
 
